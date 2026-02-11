16d00c02331c5687c530060690cb1786ae6994f0
SashaWindows <sasha.daniel2006@gmail.com>
2026-02-11 19:20:16 +0100

    fixed indexerror when y + 1 > 7 and added move calc for white pawns too

--- main.py
+++ main.py
@@ -25,10 +25,17 @@ def pawn_possible_move(pos : tuple[int, int], board):
 	if board[x][y] == "p":
 			if board[x + 1][y] == "0":
 				moves_list.append([x + 1, y])
-			if (board[x + 1][y - 1] != "0"):
+			if y - 1 >= 0 and board[x + 1][y - 1] != "0":
 				moves_list.append([x + 1, y - 1])
-			if (board[x + 1][y + 1] != "0"):
+			if y + 1 <= 7 and board[x + 1][y + 1] != "0":
 				moves_list.append([x + 1, y + 1])
+	elif board[x][y] == "P":
+			if board[x - 1][y] == "0":
+				moves_list.append([x - 1, y])
+			if y - 1 >= 0 and board[x - 1][y - 1] != "0":
+				moves_list.append([x - 1, y - 1])
+			if y + 1 <= 7 and board[x - 1][y + 1] != "0":
+				moves_list.append([x - 1, y + 1])
 	return moves_list
 
 def detect_case_y(pos: tuple[int, int]):
