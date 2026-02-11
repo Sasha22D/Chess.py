e51f623969d5f7e54b4bac1687a99120cfdecf0d
SashaWindows <sasha.daniel2006@gmail.com>
2026-02-11 19:10:06 +0100

    pawn possible moves function

--- main.py
+++ main.py
@@ -1,68 +1,83 @@
 import pygame
 
-pygame.init()
 window = pygame.display.set_mode((720, 720))
 pygame.display.set_caption("Chess.py")
 clock = pygame.time.Clock()
-running = True
-
 case_size = 90
 
-board = []
-
-def	init_board():
-    global board
-    board = [
-        ["r", "n", "b", "k", "q", "b", "n", "r"],
-        ["p", "p", "p", "p", "p", "p", "p", "p"],
-        ["0", "0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0", "0"],
-        ["P", "P", "P", "P", "P", "P", "P", "P"],
-        ["R", "N", "B", "Q", "K", "B", "N", "R"]
+def	init_board(board):
+	board = [
+		["r", "n", "b", "q", "k", "b", "n", "r"],
+		["p", "p", "p", "p", "p", "p", "p", "p"],
+		["d", "0", "d", "0", "0", "0", "0", "0"],
+		["0", "0", "0", "0", "0", "0", "0", "0"],
+		["0", "0", "0", "0", "0", "0", "0", "0"],
+		["0", "0", "0", "0", "0", "0", "0", "0"],
+		["P", "P", "P", "P", "P", "P", "P", "P"],
+		["R", "N", "B", "K", "Q", "B", "N", "R"]
 	]
+	return board
 
-def detect_case_x(pos: tuple[int, int]):
-    case = pos[0]
-    i = 0
-    while (case > 0):
-        case -= case_size
-        i += 1
-    return i
+def pawn_possible_move(pos : tuple[int, int], board):
+	moves_list = []
+	y = int(pos[0] / 90)
+	x = int(pos[1] / 90)
+	if board[x][y] == "p":
+			if board[x + 1][y] == "0":
+				moves_list.append([x + 1, y])
+			if (board[x + 1][y - 1] != "0"):
+				moves_list.append([x + 1, y - 1])
+			if (board[x + 1][y + 1] != "0"):
+				moves_list.append([x + 1, y + 1])
+	return moves_list
 
 def detect_case_y(pos: tuple[int, int]):
-    case = pos[1]
-    i = 0
-    while (case > 0):
-        case -= case_size
-        i += 1
-    return i
+	case = pos[0]
+	i = 0
+	while (case > 0):
+		case -= case_size
+		i += 1
+	return i
+
+def detect_case_x(pos: tuple[int, int]):
+	case = pos[1]
+	i = 0
+	while (case > 0):
+		case -= case_size
+		i += 1
+	return i
 
 def render_board():
-    rowPair = True
-    for i in range(8):
-        if rowPair == True:
-            for j in range(8):
-                if j % 2 == 0:
-                    pygame.draw.rect(window, "beige", (i * case_size, j * case_size, case_size, case_size))
-            rowPair = False
-        elif rowPair == False:
-            for j in range(8):
-                if j % 2 != 0:
-                    pygame.draw.rect(window, "beige", (i * case_size, j * case_size, case_size, case_size))
-            rowPair = True
+	rowPair = True
+	for i in range(8):
+		if rowPair == True:
+			for j in range(8):
+				if j % 2 == 0:
+					pygame.draw.rect(window, "beige", (i * case_size, j * case_size, case_size, case_size))
+			rowPair = False
+		elif rowPair == False:
+			for j in range(8):
+				if j % 2 != 0:
+					pygame.draw.rect(window, "beige", (i * case_size, j * case_size, case_size, case_size))
+			rowPair = True
 
-while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		if event.type == pygame.MOUSEBUTTONUP:
-			pos = pygame.mouse.get_pos()
-			print(board[detect_case_y(pos) - 1][detect_case_x(pos) - 1])
-	render_board()
-	init_board()
-	pygame.display.flip()
-	clock.tick(60)
+def	main():
+	pygame.init()
+	running = True
+	board = []
 
-pygame.quit()
+	board = init_board(board)
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				pos = pygame.mouse.get_pos()
+				print(pawn_possible_move(pos, board))
+		render_board()
+		pygame.display.flip()
+		clock.tick(60)
+
+	pygame.quit()
+
+main()
