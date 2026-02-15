c5524365af0ff9c2790184779b352a38e97a46a0
SashaWindows <sasha.daniel2006@gmail.com>
2026-02-15 17:37:55 +0100

    fixed indent in rook_possible_moves()

--- main.py
+++ main.py
@@ -23,19 +23,19 @@ def pawn_possible_move(pos : tuple[int, int], board):
 	y = int(pos[0] / 90)
 	x = int(pos[1] / 90)
 	if board[x][y] == "p":
-			if board[x + 1][y] == "0":
-				moves_list.append([x + 1, y])
-			if y - 1 >= 0 and board[x + 1][y - 1] != "0":
-				moves_list.append([x + 1, y - 1])
-			if y + 1 <= 7 and board[x + 1][y + 1] != "0":
-				moves_list.append([x + 1, y + 1])
+		if board[x + 1][y] == "0":
+			moves_list.append([x + 1, y])
+		if y - 1 >= 0 and board[x + 1][y - 1] != "0":
+			moves_list.append([x + 1, y - 1])
+		if y + 1 <= 7 and board[x + 1][y + 1] != "0":
+			moves_list.append([x + 1, y + 1])
 	elif board[x][y] == "P":
-			if board[x - 1][y] == "0":
-				moves_list.append([x - 1, y])
-			if y - 1 >= 0 and board[x - 1][y - 1] != "0":
-				moves_list.append([x - 1, y - 1])
-			if y + 1 <= 7 and board[x - 1][y + 1] != "0":
-				moves_list.append([x - 1, y + 1])
+		if board[x - 1][y] == "0":
+			moves_list.append([x - 1, y])
+		if y - 1 >= 0 and board[x - 1][y - 1] != "0":
+			moves_list.append([x - 1, y - 1])
+		if y + 1 <= 7 and board[x - 1][y + 1] != "0":
+			moves_list.append([x - 1, y + 1])
 	return moves_list
 
 def	rook_possible_move(pos: tuple[int, int], board):
