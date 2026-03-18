84e7c4458e9dceb25fcb5e6eff34ecf13b937c13
SashaWindows <sasha.daniel2006@gmail.com>
2026-03-18 21:57:49 +0100

    update pawn_moves to handle when they can take a piece

--- pawn_moves.py
+++ pawn_moves.py
@@ -1,20 +1,40 @@
-def pawn_possible_moves(pos : tuple[int, int], board):
+from utils import is_white_piece, is_black_piece
+
+def black_pawn_possible_moves(pos : tuple[int, int], board):
+	moves_list = []
+	col = int(pos[0] / 90)
+	row = int(pos[1] / 90)
+
+	if board[row][col] == "P":
+		if row - 1 >= 0:
+			if board[row - 1][col] == "0":
+				moves_list.append([row - 1, col])
+			if col - 1 >= 0 and is_white_piece(row - 1, col - 1, board):
+				moves_list.append([row - 1, col - 1])
+			if col + 1 <= 7 and is_white_piece(row - 1, col + 1, board):
+				moves_list.append([row - 1, col + 1])
+	return moves_list
+
+def white_pawn_possible_moves(pos : tuple[int, int], board):
 	moves_list = []
 	col = int(pos[0] / 90)
 	row = int(pos[1] / 90)
 
 	if board[row][col] == "p":
-		if board[row + 1][col] == "0":
-			moves_list.append([row + 1, col])
-		if col - 1 >= 0 and board[row + 1][col - 1] != "0":
-			moves_list.append([row + 1, col - 1])
-		if col + 1 <= 7 and board[row + 1][col + 1] != "0":
-			moves_list.append([row + 1, col + 1])
-	elif board[row][col] == "P":
-		if board[row - 1][col] == "0":
-			moves_list.append([row - 1, col])
-		if col - 1 >= 0 and board[row - 1][col - 1] != "0":
-			moves_list.append([row - 1, col - 1])
-		if col + 1 <= 7 and board[row - 1][col + 1] != "0":
-			moves_list.append([row - 1, col + 1])
+		if row + 1 <= 7:
+			if board[row + 1][col] == "0":
+				moves_list.append([row + 1, col])
+			if col - 1 >= 0 and is_black_piece(row + 1, col - 1, board):
+				moves_list.append([row + 1, col - 1])
+			if col + 1 <= 7 and is_black_piece(row + 1, col + 1, board):
+				moves_list.append([row + 1, col + 1])
 	return moves_list
+
+def pawn_possible_moves(pos: tuple[int, int], board):
+	col = int(pos[0] / 90)
+	row = int(pos[1] / 90)
+
+	if board[row][col] == "P":
+		return black_pawn_possible_moves(pos, board)
+	else:
+		return white_pawn_possible_moves(pos, board)
