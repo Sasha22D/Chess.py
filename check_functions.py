17c52cb3542d4cac40651e946956c5fe28bf7839
SashaWindows <sasha.daniel2006@gmail.com>
2026-03-19 19:10:06 +0100

    v1 of knight and pawns check functions and update of is_check()

--- check_functions.py
+++ check_functions.py
@@ -122,18 +122,62 @@ def is_white_rook_check(row, col, board):
         return True
     return False
 
-def is_check(pos: tuple[int, int], board):
+def is_black_knight_check(row, col, board):
+    if row - 2 >= 0 and col - 1 >= 0 and board[row - 2][col - 1] == "N":
+        return True
+    if row - 2 >= 0 and col + 1 <= 7 and board[row - 2][col + 1] == "N":
+        return True
+    if row + 2 <= 7 and col + 1 <= 7 and board[row + 2][col + 1] == "N":
+        return True
+    if row + 2 <= 7 and col - 1 >= 0 and board[row + 2][col - 1] == "N":
+        return True
+    return False
+
+def is_white_knight_check(row, col, board):
+    if row - 2 >= 0 and col - 1 >= 0 and board[row - 2][col - 1] == "n":
+        return True
+    if row - 2 >= 0 and col + 1 <= 7 and board[row - 2][col + 1] == "n":
+        return True
+    if row + 2 <= 7 and col + 1 <= 7 and board[row + 2][col + 1] == "n":
+        return True
+    if row + 2 <= 7 and col - 1 >= 0 and board[row + 2][col - 1] == "n":
+        return True
+    return False
+
+def is_black_pawn_check(row, col, board):
+    if row + 1 <= 7 and col + 1 <= 7 and board[row + 1][col + 1] == "P":
+        return True
+    if row + 1 <= 7 and col - 1 >= 0 and board[row + 1][col - 1] == "P":
+        return True
+    return False
+
+def is_white_pawn_check(row, col, board):
+    if row - 1 >= 0 and col - 1 >= 0 and board[row - 1][col - 1] == "p":
+        return True
+    if row - 1 >= 0 and col + 1 <= 7 and board[row - 1][col + 1] == "p":
+        return True
+    return False
+
+def is_check(pos: tuple[int, int], board, color):
     col = int(pos[0] / 90)
     row = int(pos[1] / 90)
 
-    if board[row][col] == "K":
+    if color == "black":
         if is_white_bishop_check(row, col, board) == True:
             return True
         if is_white_rook_check(row, col, board) == True:
             return True
-    elif board[row][col] == "k":
+        if is_white_knight_check(row, col, board) == True:
+            return True
+        if is_white_pawn_check(row, col, board) == True:
+            return True
+    elif color == "white":
         if is_black_bishop_check(row, col, board) == True:
             return True
         if is_black_rook_check(row, col, board) == True:
+            return True
+        if is_black_knight_check(row, col, board) == True:
+            return True
+        if is_black_pawn_check(row, col, board) == True:
             return True
     return False
