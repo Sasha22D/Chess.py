b550742134de5f1561b65390feec4858aacdf00a
SashaWindows <sasha.daniel2006@gmail.com>
2026-03-22 21:49:33 +0100

    added knight moves calc

--- knight_moves.py
+++ knight_moves.py
@@ -13,6 +13,15 @@ def black_knight_possible_moves(pos: tuple[int, int], board):
         moves_list.append([row + 2, col + 1])
     if row + 2 <= 7 and col - 1 >= 0 and (is_white_piece(row + 2, col - 1, board) or board[row + 2][col - 1] == "0"):
         moves_list.append([row + 2, col - 1])
+
+    if col - 2 >= 0 and row - 1 >= 0 and (is_white_piece(row - 1, col - 2, board) or board[row - 1][col - 2] == "0"):
+        moves_list.append([row - 1, col - 2])
+    if col - 2 >= 0 and row + 1 <= 7 and (is_white_piece(row + 1, col - 2, board) or board[row + 1][col - 2] == "0"):
+        moves_list.append([row + 1, col - 2])
+    if col + 2 <= 7 and row + 1 <= 7 and (is_white_piece(row + 1, col + 2, board) or board[row + 1][col + 2] == "0"):
+        moves_list.append([row + 1, col + 2])
+    if col + 2 <= 7 and row - 1 >= 0 and (is_white_piece(row - 1, col + 2, board) or board[row - 1][col + 2] == "0"):
+        moves_list.append([row - 1, col + 2])
     return moves_list
 
 def white_knight_possible_moves(pos: tuple[int, int], board):
@@ -28,6 +37,15 @@ def white_knight_possible_moves(pos: tuple[int, int], board):
         moves_list.append([row + 2, col + 1])
     if row + 2 <= 7 and col - 1 >= 0 and (is_black_piece(row + 2, col - 1, board) or board[row + 2][col - 1] == "0"):
         moves_list.append([row + 2, col - 1])
+    
+    if col - 2 >= 0 and row - 1 >= 0 and (is_black_piece(row - 1, col - 2, board) or board[row - 1][col - 2] == "0"):
+        moves_list.append([row - 1, col - 2])
+    if col - 2 >= 0 and row + 1 <= 7 and (is_black_piece(row + 1, col - 2, board) or board[row + 1][col - 2] == "0"):
+        moves_list.append([row + 1, col - 2])
+    if col + 2 <= 7 and row + 1 <= 7 and (is_black_piece(row + 1, col + 2, board) or board[row + 1][col + 2] == "0"):
+        moves_list.append([row + 1, col + 2])
+    if col + 2 <= 7 and row - 1 >= 0 and (is_black_piece(row - 1, col + 2, board) or board[row - 1][col + 2] == "0"):
+        moves_list.append([row - 1, col + 2])
     return moves_list
 
 def knight_possible_moves(pos: tuple[int, int], board):
