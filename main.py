91f8ed2775fb5fa4c4520f9ac0b0b6d8f45f992c
SashaWindows <sasha.daniel2006@gmail.com>
2026-05-02 17:05:42 +0200

    feat: check legal move function and updated moves related functions

--- main.py
+++ main.py
@@ -1,49 +1,71 @@
 import pygame
+import copy
 from pawn_moves import pawn_possible_moves
 from bishop_moves import bishop_possible_moves
 from rook_moves import rook_possible_moves
 from knight_moves import knight_possible_moves
 from queen_moves import queen_possible_moves
 from king_moves import king_possible_moves
-from init_game import init_board, render_board, clock
+from init_game import init_board, render_board, clock, CASE_SIZE
+from check_functions import is_check
+from utils import is_white_piece
 
-def	detect_selected_piece(pos: tuple[int, int], board):
+def	detect_selected_piece(pos: tuple[int, int], board: list[list[str]]) -> list[list[int]]:
     moves_list = []
-    col = int(pos[0] / 90)
-    row = int(pos[1] / 90)
+    col = pos[0] // CASE_SIZE
+    row = pos[1] // CASE_SIZE
 
     if board[row][col] == "0":
         return moves_list
-    if board[row][col] == "p" or board[row][col] == "P":
+    elif board[row][col] == "p" or board[row][col] == "P":
         moves_list = pawn_possible_moves(pos, board)
-    if board[row][col] == "r" or board[row][col] == "R":
+    elif board[row][col] == "r" or board[row][col] == "R":
         moves_list = rook_possible_moves(pos, board)
-    if board[row][col] == "b" or board[row][col] == "B":
+    elif board[row][col] == "b" or board[row][col] == "B":
         moves_list = bishop_possible_moves(pos, board)
-    if board[row][col] == "n" or board[row][col] == "N":
+    elif board[row][col] == "n" or board[row][col] == "N":
         moves_list = knight_possible_moves(pos, board)
-    if board[row][col] == "q" or board[row][col] == "Q":
+    elif board[row][col] == "q" or board[row][col] == "Q":
         moves_list = queen_possible_moves(pos, board)
-    if board[row][col] == "k" or board[row][col] == "K":
+    elif board[row][col] == "k" or board[row][col] == "K":
         moves_list = king_possible_moves(pos, board)
     return moves_list
 
-def	check_move(pos: tuple[int, int], moves_list):
-    col = int(pos[0] / 90)
-    row = int(pos[1] / 90)
+def check_legal_move(pos: tuple[int, int], pos_second_click: tuple[int, int], board: list[list[str]]) -> bool:
+    col_second = pos_second_click[0] // CASE_SIZE
+    row_second = pos_second_click[1] // CASE_SIZE
+    board_copy = copy.deepcopy(board)
+    board_copy = move_selected_piece(pos, pos_second_click, board_copy)
+    is_white = is_white_piece(row_second, col_second, board_copy)
+
+    for i in range(8):
+        for j in range(8):
+            if is_white and board_copy[i][j] == "k":
+                if is_check((j * 90, i * 90), board_copy):
+                    return False
+            elif is_white == False and board_copy[i][j] == "K":
+                if is_check((j * 90, i * 90), board_copy):
+                    return False
+
+    return True
+
+def	check_move(pos: tuple[int, int], pos_second_click: tuple[int, int], moves_list, board: list[list[str]]) -> bool:
+    col = pos_second_click[0] // CASE_SIZE
+    row = pos_second_click[1] // CASE_SIZE
 
     if moves_list == None:
-        return 0
+        return False
     for move in moves_list:
         if row == move[0] and col == move[1]:
-            return 1
-    return 0
+            if check_legal_move(pos, pos_second_click, board):
+                return True
+    return False
 
 def move_selected_piece(pos: tuple[int, int], pos_second_click: tuple[int, int], board):
-    col = int(pos[0] / 90)
-    row = int(pos[1] / 90)
-    col_second = int(pos_second_click[0] / 90)
-    row_second = int(pos_second_click[1] / 90)
+    col = pos[0] // CASE_SIZE
+    row = pos[1] // CASE_SIZE
+    col_second = pos_second_click[0] // CASE_SIZE
+    row_second = pos_second_click[1] // CASE_SIZE
 
     board[row_second][col_second] = board[row][col]
     board[row][col] = "0"
@@ -57,7 +79,7 @@ def	detect_click(pos: tuple[int, int], board):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
                 pos_second_click = pygame.mouse.get_pos()
-                if (check_move(pos_second_click, moves_list) == 1):
+                if check_move(pos, pos_second_click, moves_list, board):
                     board = move_selected_piece(pos, pos_second_click, board)
                     clicked = True
                 else:
