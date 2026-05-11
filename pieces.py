5109dc88a5b9d27080e8799175da51f6bf30b197
sashalinux <sasha.daniel2006@gmail.com>
2026-05-11 21:51:25 +0200

    fix: queen get_moves and circular import

--- pieces.py
+++ pieces.py
@@ -1,12 +1,21 @@
-from board import ChessBoard
-from utils import is_empty_case
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from board import ChessBoard
 
 class Piece:
     def __init__(self, color: str, position: tuple[int, int]) -> None:
         self.color = color
         self.position = position
 
-    # def get_moves(self, board: ChessBoard) -> list[list[int]]:
+    def get_moves(self, board: ChessBoard) -> list[list[int]]:
+        raise NotImplementedError("Chaque pièce doit implémenter get_moves")
+
+    def is_enemy(self, piece: Piece) -> bool:
+        if piece == None:
+            return False
+        return self.color != piece.color
 
 class Pawn(Piece):
     def get_moves(self, board: ChessBoard) -> list[list[int]]:
@@ -14,38 +23,35 @@ class Pawn(Piece):
         row, col = self.position
         if self.color == "white":
             if row - 1 >= 0:
-                if board[row - 1][col] == "0":
+                if board.board[row - 1][col] == None:
                     moves_list.append([row - 1, col])
                 if col - 1 >= 0:
                     target = board.get_piece(row - 1, col - 1)
-                    if self._is_enemy(target):
+                    if self.is_enemy(target):
                         moves_list.append([row - 1, col - 1])
                 if col + 1 <= 7:
                     target = board.get_piece(row - 1, col + 1)
-                    if self._is_enemy(target):
+                    if self.is_enemy(target):
                         moves_list.append([row - 1, col + 1])
             if row - 2 >= 0 and row == 6:
-                if board[row - 2][col] == "0" and board[row - 1][col] == "0":
+                if board.board[row - 2][col] == None and board.board[row - 1][col] == None:
                     moves_list.append([row - 2, col])
         else:
             if row + 1 <= 7:
-                if board[row + 1][col] == "0":
+                if board.board[row + 1][col] == None:
                     moves_list.append([row + 1, col])
                 if col - 1 >= 0:
                     target = board.get_piece(row + 1, col - 1)
-                    if self._is_enemy(target):
+                    if self.is_enemy(target):
                         moves_list.append([row + 1, col - 1])
                 if col + 1 <= 7:
                     target = board.get_piece(row + 1, col + 1)
-                    if self._is_enemy(target):
+                    if self.is_enemy(target):
                         moves_list.append([row + 1, col + 1])
             if row + 2 <= 7 and row == 1:
-                if board[row + 2][col] == "0" and board[row + 1][col] == "0":
+                if board.board[row + 2][col] == None and board.board[row + 1][col] == None:
                     moves_list.append([row  + 2, col])
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
 
 class Rook(Piece):
     directions = [
@@ -63,15 +69,12 @@ class Rook(Piece):
                     moves_list.append([current_row, current_col])
                     current_row += delta_row
                     current_col += delta_col
-                elif self._is_enemy(target):
+                elif self.is_enemy(target):
                     moves_list.append([current_row, current_col])
                     break
                 else:
                     break
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
 
 class Bishop(Piece):
     directions = [
@@ -89,15 +92,12 @@ class Bishop(Piece):
                     moves_list.append([current_row, current_col])
                     current_row += delta_row
                     current_col += delta_col
-                elif self._is_enemy(target):
+                elif self.is_enemy(target):
                     moves_list.append([current_row, current_col])
                     break
                 else:
                     break
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
 
 class Knight(Piece):
     directions = [
@@ -112,12 +112,9 @@ class Knight(Piece):
             current_row, current_col = row + delta_row, col + delta_col
             if 0 <= current_row <= 7 and 0 <= current_col <= 7:
                 target = board.get_piece(current_row, current_col)
-                if target == "0" or self._is_enemy(target):
+                if target == None or self.is_enemy(target):
                     moves_list.append([current_row, current_col])
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
 
 class Queen(Piece):
     directions = [
@@ -128,12 +125,21 @@ class Queen(Piece):
 
     def get_moves(self, board: ChessBoard) -> list[list[int]]:
         moves_list = []
-        moves_list.append(Bishop.get_moves(board))
-        moves_list.append(Rook.get_moves(board))
+        row, col = self.position
+        for delta_row, delta_col in self.directions:
+            current_row, current_col = row + delta_row, col + delta_col
+            while 0 <= current_row <= 7 and 0 <= current_col <= 7:
+                target = board.get_piece(current_row, current_col)
+                if board.is_empty_case(current_row, current_col):
+                    moves_list.append([current_row, current_col])
+                    current_row += delta_row
+                    current_col += delta_col
+                elif self.is_enemy(target):
+                    moves_list.append([current_row, current_col])
+                    break
+                else:
+                    break
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
 
 class King(Piece):
     directions = [
@@ -149,11 +155,8 @@ class King(Piece):
             current_row, current_col = row + delta_row, col + delta_col
             if 0 <= current_row <= 7 and 0 <= current_col <= 7:
                 target = board.get_piece(current_row, current_col)
-                if self._is_enemy(target):
+                if self.is_enemy(target):
                     moves_list.append([current_row, current_col])
                 if board.is_empty_case(current_row, current_col):
                     moves_list.append([current_row, current_col])
         return moves_list
-
-    def _is_enemy(self, piece: Piece) -> bool:
-        return piece.color != self.color
